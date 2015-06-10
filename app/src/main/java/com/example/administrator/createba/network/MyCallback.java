0533d798e9041c64aed1c7a931cf030103decf79
Administrator <1570595476@qq.com>
2015-06-10 10:52:18 +0800

    修改  jackson  数据解析

--- app/src/main/java/com/example/administrator/createba/network/MyCallback.java
+++ app/src/main/java/com/example/administrator/createba/network/MyCallback.java
@@ -7,17 +7,20 @@ package com.example.administrator.createba.network;
 
 import android.app.Activity;
 
-import com.google.gson.Gson;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
 import java.io.IOException;
 
-public abstract class MyCallback implements Callback {
+public abstract class MyCallback<T> implements Callback {
 
     private Class aClass;
     private Activity activity;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private Object data;
 
     public MyCallback(Activity activity, Class aClass) {
         this.aClass = aClass;
@@ -38,16 +41,15 @@ public abstract class MyCallback implements Callback {
     }
 
     @Override
-    public void onResponse(Response response) throws IOException {//请求成功
+    public void onResponse(Response response) throws IOException {//请求成功 运行在工作线程中
         String json = response.body().string();
         if (aClass != null) {
-            Gson gson = new Gson();
-            gson.fromJson(json, aClass);//储存数据到实体类
+            data = objectMapper.readValues(new JsonFactory().createParser(json), aClass).next();//jackson 解析
         }
         activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                response();
+                response((T) data);
             }
         });
     }
@@ -62,7 +64,7 @@ public abstract class MyCallback implements Callback {
      * 网络请求结果、
      * 主线程
      */
-    public abstract void response();
+    public abstract void response(T data);
 
 
 }
